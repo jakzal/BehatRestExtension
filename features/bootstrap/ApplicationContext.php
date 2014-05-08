b1d0290259aae74ba7c19910c3bf93a3dd153338
Jakub Zalas <jakub@zalas.pl>
2014-05-08 14:55:16 +0100

    Make sure integers are integers.

--- features/bootstrap/ApplicationContext.php
+++ features/bootstrap/ApplicationContext.php
@@ -12,6 +12,36 @@ class ApplicationContext implements SnippetAcceptingContext
     {
         $responseFile = sys_get_temp_dir().'/events';
 
-        file_put_contents($responseFile, json_encode($table->getHash()));
+        file_put_contents($responseFile, $this->toJson($table->getHash()));
+    }
+
+    /**
+     * @param array $content
+     *
+     * @return string
+     */
+    private function toJson(array $content)
+    {
+        $content = $this->fixContent($content);
+
+        return json_encode($content);
+    }
+
+    /**
+     * @param mixed $content
+     *
+     * @return mixed
+     */
+    private function fixContent($content)
+    {
+        if (is_array($content)) {
+            foreach ($content as $key => $value) {
+                $content[$key] = $this->fixContent($value);
+            }
+        } else if (preg_match('/^[0-9]+$/', $content)) {
+            $content = (int) $content;
+        }
+
+        return $content;
     }
 }
