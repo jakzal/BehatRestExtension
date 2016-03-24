29b631c40bf8958059d92ea376a8d1e86c5c9564
Jakub Zalas <jakub@zalas.pl>
2016-03-24 19:10:20 +0000

    Extract the behat runner

--- features/bootstrap/BehatRunnerContext.php
+++ features/bootstrap/BehatRunnerContext.php
@@ -3,37 +3,20 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use PHPUnit_Framework_Assert as PHPUnit;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Process\PhpExecutableFinder;
-use Symfony\Component\Process\Process;
 
 class BehatRunnerContext implements Context
 {
     /**
-     * @var string|null
+     * @var BehatRunner
      */
-    private $workingDir;
-
-    /**
-     * @var string|null
-     */
-    private $phpBin;
-
-    /**
-     * @var Process|null
-     */
-    private $process;
+    private $behatRunner;
 
     /**
      * @BeforeScenario
      */
     public function bootstrap()
     {
-        $this->workingDir = sprintf('%s/%s/', sys_get_temp_dir(), uniqid('BehatRestExtension_'));
-        $this->getFilesystem()->mkdir($this->workingDir, 0777);
-
-        $this->phpBin = $this->findPhpBinary();
-        $this->process = new Process(null);
+        $this->behatRunner = new BehatRunner(sprintf('%s/%s/', sys_get_temp_dir(), uniqid('BehatRestExtension_')));
     }
 
     /**
@@ -41,7 +24,7 @@ class BehatRunnerContext implements Context
      */
     public function removeWorkDir()
     {
-        $this->getFilesystem()->remove($this->workingDir);
+        $this->behatRunner->removeWorkspace();
     }
 
     /**
@@ -49,7 +32,7 @@ class BehatRunnerContext implements Context
      */
     public function givenBehatConfiguration(PyStringNode $content)
     {
-        $this->getFilesystem()->dumpFile($this->workingDir.'/behat.yml', $content->getRaw());
+        $this->behatRunner->dumpFile('behat.yml', $content->getRaw());
     }
 
     /**
@@ -57,7 +40,7 @@ class BehatRunnerContext implements Context
      */
     public function aContextFileNamedWith($fileName, PyStringNode $content)
     {
-        $this->getFilesystem()->dumpFile($this->workingDir.'/'.$fileName, $content->getRaw());
+        $this->behatRunner->dumpFile($fileName, $content->getRaw());
     }
 
     /**
@@ -65,17 +48,7 @@ class BehatRunnerContext implements Context
      */
     public function iRunBehat()
     {
-        $this->process->setWorkingDirectory($this->workingDir);
-        $this->process->setCommandLine(
-            sprintf(
-                '%s %s %s',
-                $this->phpBin,
-                escapeshellarg(BEHAT_BIN_PATH),
-                strtr('--format-settings=\'{"timer": false}\'', array('\'' => '"', '"' => '\"'))
-            )
-        );
-        $this->process->start();
-        $this->process->wait();
+        $this->behatRunner->run();
     }
 
     /**
@@ -84,9 +57,9 @@ class BehatRunnerContext implements Context
     public function itShouldPass()
     {
         try {
-            PHPUnit::assertSame(0, $this->process->getExitCode(), 'Command terminated with an error');
+            PHPUnit::assertSame(0, $this->behatRunner->getExitCode(), 'Command terminated with an error');
         } catch (\Exception $e) {
-            echo $this->getOutput();
+            echo $this->behatRunner->getFullOutput();
 
             throw $e;
         }
@@ -98,9 +71,9 @@ class BehatRunnerContext implements Context
     public function itShouldFail()
     {
         try {
-            PHPUnit::assertNotSame(0, $this->process->getExitCode(), 'Command succeeded');
+            PHPUnit::assertNotSame(0, $this->behatRunner->getExitCode(), 'Command succeeded');
         } catch (\Exception $e) {
-            echo $this->getOutput();
+            echo $this->behatRunner->getFullOutput();
 
             throw $e;
         }
@@ -114,7 +87,7 @@ class BehatRunnerContext implements Context
         $this->itShouldPass();
 
         foreach ($expectedOutput->getStrings() as $expectedLine) {
-            PHPUnit::assertRegExp('/'.preg_quote($expectedLine, '/').'/sm', $this->getOutput());
+            PHPUnit::assertRegExp('/'.preg_quote($expectedLine, '/').'/sm', $this->behatRunner->getFullOutput());
         }
     }
 
@@ -126,38 +99,7 @@ class BehatRunnerContext implements Context
         $this->itShouldFail();
 
         foreach ($expectedOutput->getStrings() as $expectedLine) {
-            PHPUnit::assertRegExp('/'.preg_quote($expectedLine, '/').'/sm', $this->getOutput());
+            PHPUnit::assertRegExp('/'.preg_quote($expectedLine, '/').'/sm', $this->behatRunner->getFullOutput());
         }
-    }
-
-    /**
-     * @return string
-     */
-    private function getOutput()
-    {
-        return $this->process->getErrorOutput().$this->process->getOutput();
-    }
-
-    /**
-     * @return Filesystem
-     */
-    private function getFilesystem()
-    {
-        return new Filesystem();
-    }
-
-    /**
-     * @return string
-     * @throws RuntimeException
-     */
-    private function findPhpBinary()
-    {
-        $phpFinder = new PhpExecutableFinder();
-
-        if (false === $php = $phpFinder->find()) {
-            throw new \RuntimeException('Unable to find the PHP executable.');
-        }
-
-        return $php;
     }
 }
